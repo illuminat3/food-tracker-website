8535294d9f7cd2db1087c01df26bd27334f24c26
Matthew <matthewrawson2@gmail.com>
2024-07-02 22:41:45 +0100

    Added Error Type

---
+++ FoodTracker/Data/DTOs/Transfer/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace FoodTracker.Data.DTOs.Transfer;
+
+public class Error (bool isActive, string? message = null)
+{
+    public bool IsActive { get; set; } = isActive;
+    public string? Message { get; set; } = message;
+}
