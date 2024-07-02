510e319b0826c056fe46b80fcae5bb2354fe9e3e
Matthew Rawson <mrawson@redev.co.uk>
2024-07-02 17:50:04 +0100

    Added Db Context

---
+++ FoodTracker/DbContext/FoodTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿using FoodTracker.Data.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodTracker.DbContext;
+
+public class FoodTrackerContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public FoodTrackerContext(DbContextOptions<FoodTrackerContext> options) : base(options)
+    {
+    }
+}
