e92f05e3ae8b6c51b03f1430731e83cdfc5470d7
Matthew <matthewrawson2@gmail.com>
2024-07-02 22:37:08 +0100

    Created UnsecureComponentBase to stop logged in users from signing in again

---
+++ FoodTracker/Pages/BaseComponents/UnsecureComponentBase.cs
@@ -0,0 +1,31 @@
+﻿using FoodTracker.Data.DTOs;
+using FoodTracker.Service.DataServices.Abstraction;
+using Microsoft.AspNetCore.Components;
+
+namespace FoodTracker.Pages.BaseComponents;
+
+public class UnsecureComponentBase : ComponentBase
+{
+    [Inject]
+    protected IUserService UserService { get; set; }
+
+    [Inject]
+    protected NavigationManager Navigation { get; set; }
+    
+    [Inject]
+    protected ILogger<UnsecureComponentBase> Logger { get; set; }
+
+    private User? CurrentUser { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Logger.LogInformation("OnInitializedAsync called in UnsecureComponentBase.");
+        
+        CurrentUser = await UserService.GetCurrentUser();
+        if (CurrentUser != null)
+        {
+            Logger.LogInformation("CurrentUser exists. Stopping them from viewing this page.");
+            Navigation.NavigateTo("/");
+        }
+    }
+}
