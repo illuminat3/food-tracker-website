ae9360f3312f5228f223769053a99c87c2b305f2
Matthew <matthewrawson2@gmail.com>
2024-07-02 22:35:58 +0100

    Moved Files

---
+++ FoodTracker/Data/DTOs/Transfer/Response.cs
@@ -0,0 +1,7 @@
+﻿namespace FoodTracker.Data.DTOs.Transfer;
+
+public class Response(bool isSuccess, string? message = null)
+{
+    public bool IsSuccess { get; set; } = isSuccess;
+    public string? Message { get; set; } = message;
+}
