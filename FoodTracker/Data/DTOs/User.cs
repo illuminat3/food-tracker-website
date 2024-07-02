b17ae4e931b241fbb6376255dbca7cf47472b597
Matthew Rawson <mrawson@redev.co.uk>
2024-07-02 17:49:14 +0100

    Updated to fit efcore

--- FoodTracker/Data/DTOs/User.cs
+++ FoodTracker/Data/DTOs/User.cs
@@ -1,9 +1,19 @@
-﻿namespace FoodTracker.Data.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class User
+namespace FoodTracker.Data.DTOs
 {
-    public string Id { get; set; } = new Guid().ToString();
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string HashedPassword { get; set; }
+    public class User
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string HashedPassword { get; set; }
+    }
 }
