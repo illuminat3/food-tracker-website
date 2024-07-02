7e2131e5b24b29214a25189d021c11116c384099
Matthew <matthewrawson2@gmail.com>
2024-07-02 22:36:40 +0100

    Added Logging to SecureComponentBase.cs

--- FoodTracker/Pages/BaseComponents/SecureComponentBase.cs
+++ FoodTracker/Pages/BaseComponents/SecureComponentBase.cs
@@ -11,15 +11,25 @@ public class SecureComponentBase : ComponentBase
 
     [Inject]
     protected NavigationManager Navigation { get; set; }
+    
+    [Inject]
+    protected ILogger<SecureComponentBase> Logger { get; set; }
 
     private User? CurrentUser { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+        Logger.LogInformation("OnInitializedAsync called in SecureComponentBase.");
+        
         CurrentUser = await UserService.GetCurrentUser();
         if (CurrentUser == null)
         {
+            Logger.LogInformation("CurrentUser is null, navigating to login.");
             Navigation.NavigateTo("/login");
+        }
+        else
+        {
+            Logger.LogInformation($"CurrentUser: {CurrentUser.Username}");
         }
     }
 }
