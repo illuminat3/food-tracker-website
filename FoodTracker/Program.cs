925fc3f9db7ee51d4c74275101abaaccdb8977cb
Matthew <matthewrawson2@gmail.com>
2024-07-02 22:40:01 +0100

    Added program.cs injections

--- FoodTracker/Program.cs
+++ FoodTracker/Program.cs
@@ -6,7 +6,6 @@ using FoodTracker.Service.DataServices.DataAccess.Abstraction;
 using FoodTracker.Service.FunctionalServices;
 using FoodTracker.Service.FunctionalServices.Abstraction;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,10 +16,20 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<FoodTrackerContext>(options =>
     options.UseSqlite("Data Source=database/database.db"));
 
+// Register application services
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserDataAccess, UserDataAccess>();
 builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<IRegisterService, RegisterService>();
+builder.Services.AddSingleton<IHashService, HashService>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+// Configure logging
+builder.Services.AddLogging(config =>
+{
+    config.AddConsole();
+    config.AddDebug();
+});
 
 // Configure Kestrel to use port 500 - must be http or causes issues
 builder.WebHost.UseUrls("http://*:500");
@@ -35,7 +44,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
