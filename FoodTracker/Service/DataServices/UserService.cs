2211523b18f0938a9b740666015320b4f7967b19
Matthew Rawson <mrawson@redev.co.uk>
2024-07-03 10:10:20 +0100

    Added Robust Local Storage implementation

--- FoodTracker/Service/DataServices/UserService.cs
+++ FoodTracker/Service/DataServices/UserService.cs
@@ -1,27 +1,86 @@
-﻿using FoodTracker.Data.DTOs;
+﻿using Blazored.LocalStorage;
+using FoodTracker.Data.DTOs;
 using FoodTracker.Service.DataServices.Abstraction;
+using System.Text.Json;
+using FoodTracker.Service.DataServices.DataAccess.Abstraction;
 
-namespace FoodTracker.Service.DataServices;
-
-public class UserService : IUserService
+namespace FoodTracker.Service.DataServices
 {
-    private User? _user;
-    
-    public Task<User?> GetCurrentUser()
+    public class UserService : IUserService
     {
-        return Task.FromResult(_user);
-    }
-
-    public Task<bool> SetCurrentUser(User? user)
-    {
-        try
+        private readonly ILocalStorageService _localStorage;
+        private readonly IUserDataAccess _userDataAccess;
+        private const string UserKey = "foodTrackerCurrentUser";
+        private User? _user;
+        
+        public UserService(ILocalStorageService localStorage, IUserDataAccess userDataAccess)
         {
-            _user = user;
-            return Task.FromResult(true);
+            _localStorage = localStorage;
+            _userDataAccess = userDataAccess;
         }
-        catch
+        
+        public async Task<User?> GetCurrentUser()
         {
-            return Task.FromResult(false);
+            try
+            {
+                if (_user != null)
+                {
+                    return _user;
+                }
+                
+                var userJson = await _localStorage.GetItemAsStringAsync(UserKey);
+                
+                if (string.IsNullOrEmpty(userJson))
+                {
+                    return null;
+                }
+                
+                var user = JsonSerializer.Deserialize<User>(userJson);
+                
+                if (user == null)
+                {
+                    return null;
+                }
+                
+                var tempUser = await _userDataAccess.Get(user.Id);
+                
+                if (tempUser == null)
+                {
+                    return null;
+                }
+                
+                if (user.IsMatch(tempUser))
+                {
+                    _user = tempUser;
+                }
+
+                return _user;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> SetCurrentUser(User? user)
+        {
+            try
+            {
+                if (user == null)
+                {
+                    await _localStorage.RemoveItemAsync(UserKey);
+                }
+                else
+                {
+                    var userJson = JsonSerializer.Serialize(user);
+                    await _localStorage.SetItemAsStringAsync(UserKey, userJson);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
