1837fcdbef86a8ea651f96aeab2f1e8d84f15a69
Matthew Rawson <mrawson@redev.co.uk>
2024-07-03 10:59:25 +0100

    Added Admin Properties

--- FoodTracker/Data/DTOs/Database/User.cs
+++ FoodTracker/Data/DTOs/Database/User.cs
@@ -4,17 +4,11 @@ namespace FoodTracker.Data.DTOs
 {
     public class User
     {
-        [Key]
-        public string Id { get; set; } = Guid.NewGuid().ToString();
-
-        [Required]
-        public string? Username { get; set; }
-
-        [Required]
-        public string? Email { get; set; }
-
-        [Required]
-        public string? HashedPassword { get; set; }
+        [Key] public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required] public string? Username { get; set; }
+        [Required] public string? Email { get; set; }
+        [Required] public string? HashedPassword { get; set; }
+        [Required] public bool IsAdmin { get; set; } = false;
 
         public bool IsMatch(User user)
         {
---
+++ FoodTracker/Pages/BaseComponents/AdminComponentBase.cs
@@ -0,0 +1,40 @@
+﻿using FoodTracker.Data.DTOs;
+using FoodTracker.Service.DataServices.Abstraction;
+using Microsoft.AspNetCore.Components;
+
+namespace FoodTracker.Pages.BaseComponents;
+
+public class AdminComponentBase : ComponentBase
+{
+    [Inject]
+    protected IUserService UserService { get; set; }
+
+    [Inject]
+    protected NavigationManager Navigation { get; set; }
+    
+    [Inject]
+    protected ILogger<AdminComponentBase> Logger { get; set; }
+
+    private User? CurrentUser { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Logger.LogInformation("OnInitializedAsync called in AdminComponentBase.");
+        
+        CurrentUser = await UserService.GetCurrentUser();
+        if (CurrentUser == null)
+        {
+            Logger.LogInformation("CurrentUser is null, navigating to login.");
+            Navigation.NavigateTo("/login");
+        }
+        else if (!CurrentUser.IsAdmin)
+        {
+            Logger.LogInformation($"CurrentUser is not admin: {CurrentUser.Username}");
+            Navigation.NavigateTo("/");
+        }
+        else
+        {
+            Logger.LogInformation($"CurrentUser is admin: {CurrentUser.Username}");
+        }
+    }
+}
