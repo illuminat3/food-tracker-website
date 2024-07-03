d24e0453512731f1d39de93d46afbc175c830375
Matthew Rawson <mrawson@redev.co.uk>
2024-07-03 10:25:41 +0100

    Added a button to go to the login page

--- FoodTracker/Pages/Register.razor.cs
+++ FoodTracker/Pages/Register.razor.cs
@@ -56,4 +56,10 @@ public partial class Register
             _error.Message = response.Message;
         }
     }
+
+    private void NavigateToLogin()
+    {
+        _navigationManager.NavigateTo("/login");
+    }
+    
 }
