aaa79c25b39cfc5833be94532ba9258620de8b1c
Matthew Rawson <mrawson@redev.co.uk>
2024-07-03 10:25:28 +0100

    Added a button to go to the register page

--- FoodTracker/Pages/Login.razor.cs
+++ FoodTracker/Pages/Login.razor.cs
@@ -1,6 +1,13 @@
-﻿namespace FoodTracker.Pages;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FoodTracker.Pages;
 
 public partial class Login
 {
+    [Inject] private NavigationManager _navigationManager {get; set;}
     
+    private void NavigateToRegister()
+    {
+        _navigationManager.NavigateTo("/register");
+    }
 }
